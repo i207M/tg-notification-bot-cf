7f8e79cb24b3bc0f0a3f68a7f39e5af3620b093d
i207M <connect@mail.i207m.top>
2022-07-24 15:57:46 +0800

    done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Cloudflare提醒机器人
+# Cloudflare 提醒机器人
 
-借助Cloudflare，通过HTTP请求来召唤Telegram bot发送提醒。
+A Telegram bot running on Cloudflare workers that send a message to you, summoned via an HTTP request.
+
+借助 Cloudflare，通过 HTTP 请求来召唤 Telegram bot 发送提醒。
--- src/index.js
+++ src/index.js
@@ -9,7 +9,26 @@
  */
 
 export default {
-	async fetch(request) {
-		return new Response("Hello World!");
-	},
+  async fetch(request, env, context) {
+    const url = new URL(request.url);
+
+    if (!url.searchParams.has('msg') || !url.searchParams.has('token')) {
+      return new Response('Missing parameters', { status: 400 });
+    }
+
+    if (url.searchParams.get('token') != env.TOKEN) {
+      return new Response('Invalid Token', { status: 401 });
+    }
+
+    const msg = url.searchParams.get('msg');
+
+    const bot_req = new Request(
+      `https://api.telegram.org/bot${env.TG_BOT_TOKEN}/sendMessage?chat_id=${env.TG_CHAT_ID}&text=${msg}`
+    );
+    const bot_res = await fetch(bot_req);
+    return new Response(bot_res.body, {
+      status: bot_res.status,
+      headers: bot_res.headers,
+    });
+  },
 };
